9dbaf328e8842f82141ef58b60b16b68591893d7
IT1050-AaronOsowski <aaron.osowski@yahoo.com>
2017-10-25 19:32:37 -0400

    lab09

---
+++ Lab09/MyMath.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab09
+{
+    
+    class MyMath
+    {
+        double result;
+        double operand1;
+        double operand2;
+
+
+        public MyMath()
+        {
+        }
+
+
+        public void Multiply(double operand1, double operand2, out string sign)
+        {
+            result = operand1 * operand2;
+            sign = "*";
+
+        }
+
+        public void Divide(double operand1, double operand2, out string sign)
+        {
+            result = operand1 / operand2;
+            sign = "/";
+        }
+
+        public void Subtract(double operand1, double operand2, out string sign)
+        {
+            result = operand1 - operand2;
+            sign = "-";
+
+        }
+
+        public void Add(double operand1, double operand2, out string sign)
+        {
+            result = operand1 + operand2;
+            sign = "+";
+
+        }
+
+        public double GetResult()
+        {
+            return result;
+        }
+
+    }
+}
--- Lab09/Program.cs
+++ Lab09/Program.cs
@@ -6,7 +6,33 @@ namespace Lab09
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyMath m = new MyMath();
+            Random r = new Random();
+
+            string message = "Ya done some math." +
+                "\n" +
+                "Your result is: ";
+            string sign = "";
+
+            double operand1 = r.Next(100);
+            double operand2 = r.Next(100);
+
+            Console.WriteLine("Your numbers are [{0}] and [{1}]\n", operand1, operand2);
+
+            m.Multiply(operand1, operand2, out sign);
+            Console.WriteLine("{0}{1}   [[{2} {3} {4}]]\n",message, m.GetResult(),
+                operand1, sign, operand2);
+            m.Divide(operand1, operand2, out sign);
+            Console.WriteLine("{0}{1}   [[{2} {3} {4}]]\n", message, m.GetResult(),
+                operand1, sign, operand2);
+            m.Subtract(operand1, operand2, out sign);
+            Console.WriteLine("{0}{1}   [[{2} {3} {4}]]\n", message, m.GetResult(),
+                 operand1, sign, operand2);
+            m.Add(operand1, operand2, out sign);
+            Console.WriteLine("{0}{1}   [[{2} {3} {4}]]\n", message, m.GetResult(),
+                operand1, sign, operand2);
+
+            Console.ReadKey(true);
         }
     }
 }
